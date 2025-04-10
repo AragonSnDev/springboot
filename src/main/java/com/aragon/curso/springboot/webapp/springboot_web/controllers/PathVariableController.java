4f51ebeb35e8774a312426546daeb75290124abf
Luis Daniel Aragon Sanchez <luis.aragons@coppel.com>
2025-04-10 15:24:18 -0700

    [ADD] Primera peticion post

--- src/main/java/com/aragon/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/aragon/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,10 +1,16 @@
 package com.aragon.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aragon.curso.springboot.webapp.springboot_web.models.User;
 import com.aragon.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 @RestController
@@ -16,4 +22,22 @@ public class PathVariableController {
         param.setMessage(message);
         return param;
     }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String ,Object> mixPathVar(@PathVariable String product, @PathVariable Long id ){
+        Map<String,Object> json = new HashMap<>();
+
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        //hacer algo con el usuario en la base de datos
+
+        return user;
+    }
+
 }
